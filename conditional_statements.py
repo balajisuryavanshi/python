43094e148e1f0dbdc1e9d6752c432d4c4b59c16a
balajisuryavanshi <159866189+balajisuryavanshi@users.noreply.github.com>
2024-08-21 19:31:42 +0530

    conditional_statements.py

---
+++ conditional_statements.py
@@ -0,0 +1,38 @@
+# if statements
+a = 10
+b = 5
+if a > b:
+ print("a is greather than b")
+
+ #if_else statements
+ a = 10
+ b = 20
+if a > b:
+ print("a is greather than b")
+else:
+ print("b is greather than a")
+ 
+n = 4
+if n%2==0:
+    print("number is even")
+else:
+    print("number is odd")
+
+#if..elif....else statements
+a = 4
+b = 4
+if a > b:
+    print("a is greather than b")
+elif a == b:
+    print("a and b are same")
+else:
+    print("b is greather than a")
+
+#nested if_else statements
+a = -10
+if a > 0:
+    print("number is positive")
+else:
+    print("number is negative")
+if -10 <= a:
+    print("two digit negative number")
