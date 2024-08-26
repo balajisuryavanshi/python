84f61dba9bbc1521c585888cade8aab483965d40
balajisuryavanshi <159866189+balajisuryavanshi@users.noreply.github.com>
2024-08-26 19:56:28 +0530

    trafficlight.py

---
+++ trafficlight.py
@@ -0,0 +1,10 @@
+light = input("traffic light :")
+
+if(light == "red"):
+    print("Stop")
+elif(light == "yellow"):
+    print("See")
+elif(light == "green"):
+    print("Go")
+else:
+    print("Light Is Off")
