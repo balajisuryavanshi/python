cb5ca78f4173b0b232e4f2ae1b774b87496244b9
balajisuryavanshi <159866189+balajisuryavanshi@users.noreply.github.com>
2024-10-09 09:52:31 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    result = a+b
+    return result
+def sub(a,b):
+    result = a-b
+    return result
+def mul(a=5,b=10):
+    result = a*b
+    return result
---
+++ module1.py
@@ -0,0 +1,4 @@
+import module
+print("addition=" , module.add(10,20))
+print("substract" , module.sub(10,20))
+print("multiple" , module.mul())
